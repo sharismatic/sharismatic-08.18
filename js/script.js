87ce6c3c42100c53f9513baea903c432812e68ee
sharismatic <sharismatic@gmail.com>
2013-09-22 11:47:38 -0400

    STICKY NAV WORKING

--- js/script.js
+++ js/script.js
@@ -3,11 +3,24 @@ function initAll() {
 	initAjax();
 	contact();
 	setUpForm();
-	nav_fade();
 	
 }
 
 initAll();
+
+
+
+// $(window).scroll(function(){
+ 
+// if ($(this).scrollTop() > 65) {
+//     $('#fademenu').fadeTo( 300, 1 );
+// }
+// else {
+// 	$('#fademenu').fadeTo( 300, 0.0 );
+// }
+ 
+
+// });
 
 function initHovers() {
 	// find all .panels inside .work1 and show/hide
@@ -86,127 +99,121 @@ function perform_close() {
 	});
 
 }
+function contact(){
+	$("#contact").click(function(){
+		$("#form").toggle('slow');
 
-function nav_fade(){
+		return false;
+	});
+}
+
+
+function validate_form(){
 	
 
+	$('#form .contact_form span').hide();
+
+	//save input values as variables
+	var name = $("#name").val();
+	var email= $ ("#emailFrom").val();
+	var phone = $("#phone").val();
+	var comments = $("#emailText").val();
+
+	//create variable for the total number of errors
+	var num_errors = 0;
+
+	//validate name
+	if(name.length < 1){
+
+		$("#name_error").show();
+		num_errors = num_errors + 1;
+
+	}
+
+	if(comments.length < 1){
+
+		$("#comment_error").show();
+		num_errors ++;
+
+	}
+
+
+
+	var regex = /^\d{3}-?\d{3}-?\d{4}$/g;
+	if (regex.test(phone) == false){
+		$("#user_ph_error").show();
+		num_errors ++;
+	}
+
+	var email_regex = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/
+	if (email_regex.test(email)== false){
+		$("#user_email_error").show();
+		num_errors ++;
+	}
+	console.log( '########## EMAIL HERE')
+	console.log( email )
+
+	if(num_errors == 0){
+		return {
+			name: name,
+			email: email,
+			phone: phone,
+			comments: comments
+		};
+		//submit_form (name, age ,phone, email);
+	} else {
+		return -1;
+	}
+}
+
+function setUpForm() {
+	// METHOD: set up contact form ajax
+	var form = $('.contact_form');
+
+	if ( form.length == 0 ) return;
+
+	// if we made it here, kosher to send ajax
+	form.submit( function() {
+		var isValid = validate_form();
+
+		if ( isValid == -1 ) return false;
+		
+		// if we are here, safe to make AJAX request
+		var dataObj = {
+			name: isValid.email
+			, emailFrom: isValid.email
+			, phone: isValid.phone
+			, emailText: isValid.comments
+		}
+
+		console.log( '######### setUpForm');
+		console.log( dataObj )
+
+		$.ajax(
+		{
+			type: "POST",
+			url: EMAIL_URL,
+			data: {
+				name: isValid.email
+				, emailFrom: isValid.email
+				, phone: isValid.phone
+				, emailText: isValid.comments
+
+			},
+			
+			success: function( data ) {
+				console.log( data )
+				$('#form').empty().append('<span style= "text-align: center;">Message sent!</span>');
+			},
+			error: function( textStatus ) {
+				console.log( textStatus.responseText );
+			}
+		}
+		);	
+		return false;
+	});
 
 }
-// function contact(){
-// 	$("#contact").click(function(){
-// 		$("#form").toggle('slow');
-
-// 		return false;
-// 	});
-// }
-
-
-// function validate_form(){
-	
-
-// 	$('#form .contact_form span').hide();
-
-// 	//save input values as variables
-// 	var name = $("#name").val();
-// 	var email= $ ("#emailFrom").val();
-// 	var phone = $("#phone").val();
-// 	var comments = $("#emailText").val();
-
-// 	//create variable for the total number of errors
-// 	var num_errors = 0;
-
-// 	//validate name
-// 	if(name.length < 1){
-
-// 		$("#name_error").show();
-// 		num_errors = num_errors + 1;
-
-// 	}
-
-// 	if(comments.length < 1){
-
-// 		$("#comment_error").show();
-// 		num_errors ++;
-
-// 	}
-
-
-
-// 	var regex = /^\d{3}-?\d{3}-?\d{4}$/g;
-// 	if (regex.test(phone) == false){
-// 		$("#user_ph_error").show();
-// 		num_errors ++;
-// 	}
-
-// 	var email_regex = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/
-// 	if (email_regex.test(email)== false){
-// 		$("#user_email_error").show();
-// 		num_errors ++;
-// 	}
-// 	console.log( '########## EMAIL HERE')
-// 	console.log( email )
-
-// 	if(num_errors == 0){
-// 		return {
-// 			name: name,
-// 			email: email,
-// 			phone: phone,
-// 			comments: comments
-// 		};
-// 		//submit_form (name, age ,phone, email);
-// 	} else {
-// 		return -1;
-// 	}
-// }
-
-// function setUpForm() {
-// 	// METHOD: set up contact form ajax
-// 	var form = $('.contact_form');
-
-// 	if ( form.length == 0 ) return;
-
-// 	// if we made it here, kosher to send ajax
-// 	form.submit( function() {
-// 		var isValid = validate_form();
-
-// 		if ( isValid == -1 ) return false;
-		
-// 		// if we are here, safe to make AJAX request
-// 		var dataObj = {
-// 			name: isValid.email
-// 			, emailFrom: isValid.email
-// 			, phone: isValid.phone
-// 			, emailText: isValid.comments
-// 		}
-
-// 		console.log( '######### setUpForm');
-// 		console.log( dataObj )
-
-// 		$.ajax(
-// 		{
-// 			type: "POST",
-// 			url: EMAIL_URL,
-// 			data: {
-// 				name: isValid.email
-// 				, emailFrom: isValid.email
-// 				, phone: isValid.phone
-// 				, emailText: isValid.comments
-
-// 			},
-			
-// 			success: function( data ) {
-// 				console.log( data )
-// 				$('#form').empty().append('<span style= "text-align: center;">Message sent!</span>');
-// 			},
-// 			error: function( textStatus ) {
-// 				console.log( textStatus.responseText );
-// 			}
-// 		}
-// 		);	
-// 		return false;
-// 	});
-
-// }
 // HOW TO SELECT DATA ATTRIBUTE
 // $('input[data-url='+URL FRAGMENT+']')
 
@@ -306,3 +313,17 @@ History.Adapter.bind(window,'statechange',function(){ // Note: We are using stat
     //var State = History.getState(); // Note: We are using History.getState() instead of event.state
     _check_change();
 });
+
+
+$(window).scroll(function(){
+ 
+if ($(this).scrollTop() > 40) {
+    $('#fademenu').css({display: "block"}).animate({opacity: "1"}, 500);
+}
+else {
+	$('#fademenu').css({display: "none"}).animate( {opacity: "0"}, 500);
+}
+ 
+
+});
+
